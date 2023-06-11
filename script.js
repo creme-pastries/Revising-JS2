6268f5891b4bacc685190dd077fd87a951851606
JOSH <onyemajoshua892@gmail.com>
2023-06-11 19:30:40 +0100

    Rest Pattern and Parameters

--- script.js
+++ script.js
@@ -44,8 +44,48 @@ const restaurant = {
   orderPasta: function (ing1, ing2, ing3) {
     console.log(`Here is your delicious pasta with ${ing1}, ${ing2}, ${ing3}`);
   },
+  orderPizza: function (mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient);
+    console.log(otherIngredients);
+  },
 };
 
+//Spread , beacuse of Right side of ==
+const arr = [1, 2, ...[3, 4]];
+
+// Rest, becasue on Left
+const [a, b, ...others] = [1, 2, 3, 4, 5];
+console.log(a, b, others);
+
+const [pizza, , risotto, ...otherFood] = [
+  ...restaurant.mainMenu,
+  ...restaurant.starterMenu,
+];
+
+console.log(pizza, risotto, otherFood);
+
+//objects
+const { sat, ...weekdays } = restaurant.openingHours;
+weekdays;
+
+// 2 Functions
+
+const add = function (...numbers) {
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) sum += numbers[i];
+
+  console.log(sum);
+};
+add(2, 3);
+add(9, 4, 5, 2);
+add(2, 3, 5, 7, 7, 1, 3, 4);
+
+const x = [2, 34, 5];
+add(...x);
+
+restaurant.orderPizza('mushrooms', 'onion', 'olives', 'spinach');
+restaurant.orderPizza('mushrooms');
+/*
 const arr = [7, 8, 9];
 const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
 console.log(badNewArr);
@@ -87,7 +127,7 @@ const restaurantCopy = { ...restaurant };
 restaurantCopy.name = 'Ristorante Roma';
 console.log(restaurantCopy.name);
 console.log(restaurant.name);
-
+*/
 /*
 restaurant.orderDelivery({
   time: '22:30',
