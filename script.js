433eccc1fe542393cd55aef7fe065fca16d68ab6
JOSH <onyemajoshua892@gmail.com>
2023-06-12 14:15:50 +0100

    Maps: Fundamentals

--- script.js
+++ script.js
@@ -47,6 +47,38 @@ const restaurant = {
   },
 };
 
+const rest = new Map();
+rest.set('name', 'Classico Italiano');
+rest.set(1, 'Firenze, Italy');
+rest.set(2, 'Lisbon, Portugal');
+rest;
+
+rest
+  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'We are open :D')
+  .set(false, 'We are closed :C');
+
+console.log(rest.get('name'));
+console.log(rest.get(true));
+console.log(rest.get(1));
+
+const time = 21;
+
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+console.log(rest.has('categories'));
+rest.delete(2);
+const arr = [1, 2];
+rest.set(arr, 'Test');
+// rest.set(document.querySelector('h1'), 'Heading')
+rest;
+// rest.clear();
+console.log(rest.size);
+
+console.log(rest.get(arr));
+/**
 const ordersSet = new Set([
   'Pasta',
   'Pizza',
@@ -80,7 +112,7 @@ console.log(
 console.log(new Set('thequickbrownfoxjumpsoverthelazydog').size);
 
 // Coding Challenge 2
-/**
+
  * Let's continue with our football betting app! Keep using the 'game' variable from 
 before.
 Your tasks:
